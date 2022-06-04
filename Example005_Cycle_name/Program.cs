ee50940df2a223c89d15c7d5dced0e5e3ef01a34
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-04 21:55:46 +0300

    Add if-else conditions

---
+++ Example005_Cycle_name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "nikita") // .ToLower() делает текст нечувствительным к регистру
+{
+    Console.WriteLine("Hello, my dear friend, Nick!");
+}
+else
+{
+    Console.Write("Hi, bustard ");
+    Console.WriteLine(username);
+}
